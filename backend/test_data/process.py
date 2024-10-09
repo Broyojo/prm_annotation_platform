6566d185c37fd489f3f8594d1e0ea852f8acced1
Broyojo <dha@xoba.com>
2024-10-09 10:58:55 -0400

    normalized existing data

---
+++ backend/test_data/process.py
@@ -0,0 +1,41 @@
+import json
+import os
+
+import nltk
+
+for file in os.listdir("./old"):
+    data = []
+
+    if file.endswith(".json"):
+        with open(os.path.join("./old", file), "r") as f:
+            data = json.load(f)
+    elif file.endswith(".jsonl"):
+        with open(os.path.join("./old", file), "r") as f:
+            data = []
+            for line in f:
+                data.append(json.loads(line))
+    else:
+        continue
+
+    new_data = []
+    for d in data:
+        if isinstance(d, str):
+            d = json.loads(json.loads(d))  # fix FRoG dataset
+        steps = nltk.sent_tokenize(d["model_answer"])
+        new_data.append(
+            {
+                "question": d["question"],
+                "answer": d["answer"],
+                "llm_answer": d["model_answer"],
+                "steps": steps,
+                "num_steps": len(steps),
+                "is_correct": d.get("is_correct"),
+                "solve_ratio": d.get("solve_ratio"),
+                "llm_name": d.get("model_name"),
+                "prompt_format": d.get("prompt_format"),
+                "final_anwer": d.get("final_answer"),
+            }
+        )
+
+    with open(os.path.join("normalized", file), "w") as f:
+        json.dump(new_data, f)
